75530556530757bd6003c6897e0b9d6dd8ba7f2e
[your <shashwatshah02@gmail.com>
2024-10-17 12:59:54 +0530

    razortpay

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ const cartRoutes = require('./routes/cartRoutes.js');
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const jwt = require('jsonwebtoken');
 const { fileURLToPath } = require("url");
+const { verifyToken } = require('./middleware/jwt.js');
 // const __filename = fileURLToPath(module.url);
 // const __dirname = path.dirname(__filename);
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const Cart = require('./models/cartModel.js');
+const env = require('dotenv').config();
+
+
+const Razorpay = require('razorpay');
+
+const razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_SECRET_KEY
+});
 
 const app = express();
 app.use(
@@ -46,7 +58,56 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Ecommerce Grocery Shopping");
 });
 
+app.post('/create-order', verifyToken, async (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1]; // Example: 'Bearer <token>'
+    console.log(token);
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized, no token provided' });
+    }
 
+    const decoded = jwt.verify(token, 'yourSecretKey');  // Replace 'your-secret-key' with your JWT secret
+    const customerId = decoded.customerId;
+    console.log(customerId);
+    const [cart] = await Cart.getCartById(customerId);
+
+    console.log([cart]); 
+    if (!cart || cart.totalPrice === undefined) {
+        return res.status(400).json({ error: 'No cart found or invalid total price' });
+    }
+
+    const totalPrice = cart.totalPrice;
+
+    const options = {
+        amount: totalPrice * 100, // amount in paise (1 INR = 100 paise)
+        currency: 'INR',
+        receipt: `receipt#${customerId}`,
+    };
+
+    try {
+        const order = await razorpayInstance.orders.create(options);
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/verify-payment', (req, res) => {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+  
+    const crypto = require('crypto');
+    const hmac = crypto.createHmac('sha256', process.env.RAZORPAY_SECRET_KEY);
+  
+    hmac.update(razorpay_order_id + '|' + razorpay_payment_id);
+    const generated_signature = hmac.digest('hex');
+  
+    if (generated_signature === razorpay_signature) {
+      // Payment is valid, proceed with order fulfillment
+      res.status(200).json({ success: true });
+    } else {
+      // Invalid payment, reject the request
+      res.status(400).json({ success: false });
+    }
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
